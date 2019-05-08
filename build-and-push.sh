be8b4a88bd30938df8c265ec05911361d2009187
Ryan Pavlik <ryan.pavlik@collabora.com>
2019-05-08 15:21:29 -0500

    Update the build script

--- build-and-push.sh
+++ build-and-push.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 # SPDX-License-Identifier: Apache-2.0
 
+set -e
+
 buildAndPush() {
     docker build . -t "$1"
     docker push "$1"
@@ -10,7 +12,10 @@ buildAndPush() {
     buildAndPush khronosgroup/docker-images
     (
         cd openxr
-        # buildAndPush khronosgroup/openxr-spec-build
-        buildAndPush rpavlik/khr-specbuilder
+        buildAndPush khronosgroup/docker-images:openxr
+    )
+    (
+        cd openxr-sdk
+        buildAndPush khronosgroup/docker-images:openxr-sdk
     )
 )
