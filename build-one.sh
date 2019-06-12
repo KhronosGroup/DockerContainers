09fcbc85fadc492f6d4d5ec74646bf40ba2d0d4e
Ryan Pavlik <ryan.pavlik@collabora.com>
2019-06-12 09:52:50 -0500

    Add a build-one script that doesn't push.

---
+++ build-one.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# SPDX-License-Identifier: Apache-2.0
+
+# Pass a dockerfile name.
+# The dockerfile's extension is used as the tag within the repo.
+# For the default dockerfile with no extension, the tag is "latest"
+
+set -e
+
+REPO="khronosgroup/docker-images"
+
+(
+    cd $(dirname $0)
+    DOCKERFILE=$1
+    TAG=${DOCKERFILE##*.}
+    if [ "$TAG" == "Dockerfile" ]; then
+        TAG=latest
+    fi
+    docker build . -f "$DOCKERFILE" -t "$REPO:$TAG"
+)
