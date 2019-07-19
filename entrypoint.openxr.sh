e90257a8f760ed3e42f10c42805fd548be16c6b5
Ryan Pavlik <ryan.pavlik@collabora.com>
2019-07-19 12:47:11 -0500

    entrypoint: Copyright statements

--- entrypoint.openxr.sh
+++ entrypoint.openxr.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+# Copyright (c) 2019 The Khronos Group, Inc.
+# Copyright (c) 2019 Collabora, Ltd
+# Copyright (c) 2017-2018 Sensics, Inc.
+# Copyright (c) 2014 Kyle Manna
+# SPDX-License-Identifier: MIT
+
+# Based in part on:
+# https://github.com/kylemanna/docker-aosp/blob/master/utils/docker_entrypoint.sh
+# https://github.com/sensics/tegra-android-builder/blob/master/content/docker_entrypoint.sh
+
 set -e
 
 # Script allows running this container
@@ -12,7 +22,7 @@ if [ -z ${GROUP_ID+x} ]; then GROUP_ID=1000; fi
 USER_GROUP=openxr_container
 
 echo "Creating user and group $USER_GROUP:$USER_GROUP with ID ${USER_ID}:${GROUP_ID}"
-groupadd -g $GROUP_ID -r $USER_GROUP && \
+groupadd -g $GROUP_ID -r $USER_GROUP > /dev/null && \
 useradd -u $USER_ID --create-home -r -g $USER_GROUP $USER_GROUP
 
 export HOME=/home/$USER_GROUP
