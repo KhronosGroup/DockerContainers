ef3afbf33d6c64a4a0449f0d07a0af94a7202e9b
Ryan Pavlik <ryan.pavlik@collabora.com>
2019-07-29 15:04:58 -0500

    Add a new base image that has no entrypoint.
    Needed by azure pipelines.

--- build-and-push-all.sh
+++ build-and-push-all.sh
@@ -6,6 +6,7 @@ set -e
 (
     cd $(dirname $0)
     ./build-and-push-one.sh Dockerfile "$@"
+    ./build-and-push-one.sh Dockerfile.openxr-base "$@"
     ./build-and-push-one.sh Dockerfile.openxr "$@"
     ./build-and-push-one.sh Dockerfile.openxr-sdk "$@"
     ./build-and-push-one.sh Dockerfile.openxr-pregenerated-sdk "$@"
