0c3c7ee6d7c529a9f90f4cfaa3a6baad15c9a91e
Ryan Pavlik <ryan.pavlik@collabora.com>
2021-02-11 13:25:17 -0600

    Version all the OpenXR images, actually.

--- build-and-push-all.sh
+++ build-and-push-all.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Copyright (c) 2019-2021, The Khronos Group Inc.
 # SPDX-License-Identifier: Apache-2.0
 
 set -e
@@ -7,9 +8,9 @@ set -e
     cd $(dirname $0)
     ./build-and-push-one.sh Dockerfile.vulkan-docs-base "$@"
     ./build-and-push-one.sh Dockerfile.vulkan-docs "$@"
-    ./build-and-push-one.sh Dockerfile.openxr-base.* "$@"
-    ./build-and-push-one.sh Dockerfile.openxr.* "$@"
-    ./build-and-push-one.sh Dockerfile.openxr-sdk "$@"
-    ./build-and-push-one.sh Dockerfile.openxr-pregenerated-sdk-base "$@"
-    ./build-and-push-one.sh Dockerfile.openxr-pregenerated-sdk "$@"
+    ./build-and-push-one.sh Dockerfile.openxr-base.202102 "$@"
+    ./build-and-push-one.sh Dockerfile.openxr.202102 "$@"
+    ./build-and-push-one.sh Dockerfile.openxr-sdk.202102 "$@"
+    ./build-and-push-one.sh Dockerfile.openxr-pregenerated-sdk-base.202102 "$@"
+    ./build-and-push-one.sh Dockerfile.openxr-pregenerated-sdk.202102 "$@"
 )
