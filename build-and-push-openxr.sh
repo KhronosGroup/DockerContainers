769be1ddbbf78cdf8667bde0ceb677e928892e05
Ryan Pavlik <ryan.pavlik@collabora.com>
2022-01-10 16:07:46 -0600

    Update the openxr pregen sdk

--- build-and-push-openxr.sh
+++ build-and-push-openxr.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 # Copyright 2019-2021, The Khronos Group Inc.
 # SPDX-License-Identifier: Apache-2.0
-
-# Pass a dockerfile name.
-# The tag portion of the name is used as the tag within the repo.
-# Name format can be either tag.dockerfile or Dockerfile.tag
-
 set -e
 
 ./build-and-push-one.sh Dockerfile.openxr-base.*
 ./build-and-push-one.sh Dockerfile.openxr.*
 ./build-and-push-one.sh Dockerfile.openxr-sdk.*
-./build-and-push-one.sh Dockerfile.openxr-pregenerated-sdk-base.*
 ./build-and-push-one.sh Dockerfile.openxr-pregenerated-sdk.*
