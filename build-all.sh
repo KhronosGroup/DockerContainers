d4e2f0c13ac6f0696d80bb6449a9f35cb3ac321c
Lőrinc Serfőző <lorinc@streamhpc.com>
2024-05-29 10:52:48 +0200

    Added OpenCL entries to build-all.sh

--- build-all.sh
+++ build-all.sh
@@ -7,6 +7,10 @@ set -e
 (
     cd $(dirname $0)
     ./build-one.sh asciidoctor-spec 202206 "$@"
+    ./build-one.sh opencl-sdk-base-ubuntu-20.04 20230717 "$@"
+    ./build-one.sh opencl-sdk-base-ubuntu-22.04 20230717 "$@"
+    ./build-one.sh opencl-sdk-intelcpu-ubuntu-20.04 20230717 "$@"
+    ./build-one.sh opencl-sdk-intelcpu-ubuntu-22.04 20230717 "$@"
     ./build-one.sh vulkan-docs-base 202206 "$@"
     ./build-one.sh vulkan-docs 202206 "$@"
     ./build-one.sh rust 202312 "$@"
